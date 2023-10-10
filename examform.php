f164810a4a11cd8264d7325c7052c32efbe61f51
OVHAY <ovhaykumar.cs21@bmsce.ac.in>
2023-10-10 12:00:53 +0530

    updated

--- examform.php
+++ examform.php
@@ -40,20 +40,80 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Store the semester data in the session
     $_SESSION['semester'] = $semester;
 
-    // Redirect to the reval.php page
+    // Redirect to the confirmation.php page
     header("Location: confirmation.php");
     exit;
 }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exam Form</title>
     <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f2f2f2;
+        }
+
+        header {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 10px 0;
+        }
+
+        nav {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 10px 0;
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+            margin: 0 10px;
+        }
+
+        main {
+            max-width: 800px;
+            margin: 0 auto; /* Center horizontally */
+            padding: 20px;
+            background-color: white;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            border-radius: 5px;
+            display: flex;
+            flex-direction: column;
+        
+            align-items: center; /* Center vertically */
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        label {
+            font-weight: bold;
+        }
+
+        select, input, button {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+
         table {
             border-collapse: collapse;
             width: 100%;
+            margin-bottom: 20px;
         }
 
         th, td {
@@ -67,10 +127,52 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
 
         button {
-            margin-top: 10px;
+            background-color: #333;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        button:hover {
+            background-color: #555;
+        }
+
+        @media screen and (max-width: 600px) {
+            select, input, button {
+                width: 100%;
+            }
         }
     </style>
-
+</head>
+<body>
+    <header>
+        <h1>Exam Form</h1>
+        <h1> hi</h1>
+    </header>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="examform.php">Exam Form</a>
+    </nav>
+    <main>
+        <a style="color:red" href="index.php">Logout</a>
+        <form method="POST" action="" id="form">
+            <label for="semester">Semester:</label>
+            <select id="semester" name="semester" required>
+                <option value="">Select Semester</option>
+                <?php
+                // Generate semester options dynamically
+                for ($i = 1; $i <= 8; $i++) {
+                    echo "<option value='$i'>$i</option>";
+                }
+                ?>
+            </select>
+            <button type="button" onclick="createSubjectTable()">Create Table</button>
+            <br><br>
+            <button type="submit">Submit</button>
+        </form>
+    </main>
     <script>
     function createSubjectTable() {
         var semester = document.getElementById("semester").value;
@@ -120,32 +222,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
     </script>
-</head>
-<body>
-<?php include 'header.php'; ?>
-<nav>
-    <a href="index.php">Home</a>
-    <a href="examform.php">Exam Form</a>
-</nav>
-<main>
-    <h1>Exam Form</h1>
-    <a style="color:red" href="index.php">Logout</a>
-    <form method="POST" action="" id="form">
-        <label for="semester">Semester:</label>
-        <select id="semester" name="semester" required>
-            <option value="">Select Semester</option>
-            <?php
-            // Generate semester options dynamically
-            for ($i = 1; $i <= 8; $i++) {
-                echo "<option value='$i'>$i</option>";
-            }
-            ?>
-        </select>
-        <button type="button" onclick="createSubjectTable()">Create Table</button>
-        <br><br>
-        <button type="submit">Submit</button>
-    </form>
-</main>
-<?php include 'footer.php'; ?>
 </body>
 </html>
